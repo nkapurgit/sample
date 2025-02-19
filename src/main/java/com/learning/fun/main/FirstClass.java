9947489eb489e7e85fe9084f6506b74c8cc9e95f
H003001 <nkapur@deloitte.com>
2025-02-19 15:20:28 +0530

    commit 6

--- src/main/java/com/learning/fun/main/FirstClass.java
+++ src/main/java/com/learning/fun/main/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 
     public static void main(String args[]){
         System.out.println("hello");
+        System.out.println("Galaxy");
         System.out.println("Universe");
     }
 }
