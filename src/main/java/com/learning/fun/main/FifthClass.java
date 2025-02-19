cbaf22bafceb9d4fdbf697a4678fcddddf1b6fd8
H003001 <nkapur@deloitte.com>
2025-02-19 17:44:52 +0530

    commit 4

--- src/main/java/com/learning/fun/main/FifthClass.java
+++ src/main/java/com/learning/fun/main/FifthClass.java
@@ -3,6 +3,7 @@ package com.learning.fun.main;
 public class FifthClass {
     public static void main(String args[]){
         System.out.println("hello");
+        System.out.println("Moon");
 
     }
 }
