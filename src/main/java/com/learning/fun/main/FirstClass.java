a09ca640c5780df833320b54bab4ba1cbf023383
H003001 <nkapur@deloitte.com>
2025-02-19 14:38:09 +0530

    revert commits

--- src/main/java/com/learning/fun/main/FirstClass.java
+++ src/main/java/com/learning/fun/main/FirstClass.java
@@ -4,8 +4,6 @@ public class FirstClass {
 
     public static void main(String args[]){
         System.out.println("hello");
-        System.out.println("world");
-        System.out.println("Earth");
 
     }
 }
