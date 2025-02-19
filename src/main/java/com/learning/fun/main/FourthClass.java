3575c1d9ee8f768b17e605b79fd6dcbacb185d34
H003001 <nkapur@deloitte.com>
2025-02-19 16:34:02 +0530

    commit 2

--- src/main/java/com/learning/fun/main/FourthClass.java
+++ src/main/java/com/learning/fun/main/FourthClass.java
@@ -3,6 +3,6 @@ package com.learning.fun.main;
 public class FourthClass{
         public static void main(String args[]){
             System.out.println("hello");
-
+            System.out.println("world");
         }
 }
